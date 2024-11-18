2141ccceae79b5e1f61611b649285b37d83f098e
Mohamed Agnide <fadulagnide01@gmail.com>
2024-11-18 18:24:02 +0100

    Router.php créé

---
+++ src/Controllers/TodoController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+class TodoController
+{
+
+    public function index()
+    {
+        // Récupérer les tâches depuis la session
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+        $todos = $_SESSION["todos"];
+        // charger la vue "Views/ilndex.php"
+
+        require __DIR__ . "/../Views/index.php";
+        // require dirname(path: __DIR__) . "/Views/index.php";
+    }
+    public function add() {}
+    public function delete() {}
+    public function toggle() {}
+}
